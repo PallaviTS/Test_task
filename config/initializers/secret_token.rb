a8888e445a1433eca69db289df446d38fd6ae90f
pallavi <pallavi@pallavi-work.(none)>
2013-08-06 14:42:37 +0530

    Added devise for user_login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestTask::Application.config.secret_token = '454dc0ac137a04e11cd45a6cddb7b65753e7aeefe17ad479afba2abe7c40d9c88d1e3ac018274b50798df936cc8add14795e4d88a90a0d71a2ff9018898aa941'
